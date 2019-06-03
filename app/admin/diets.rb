afc8bb3327088b049482d017547edecf87067ea5
Raghu Betina <raghu@firstdraft.com>
2019-06-03 23:05:52 +0000

    Whitelist columns in Diet for ActiveAdmin

--- app/admin/diets.rb
+++ app/admin/diets.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Diet do
+
+ permit_params :food_id, :diet_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
